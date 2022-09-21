787fa1e094c8029c360adbbd437505585a7647d6
Alexy Chrétien <awesomegameconcepts@outlook.fr>
2022-09-21 18:22:45 +0200

    Change hierachy from project type to package type

---
+++ Tests/MyTest.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTest : MonoBehaviour
+{
+    // used only for disable the unity Test assembly with no script
+    // Rename and edit this file if you want to ran Unity Test
+}
